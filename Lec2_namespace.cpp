cc3b5479f7349f5c5fd3566c15df5495b25d1800
Ayush Chaware <118629631+ayushchaware08@users.noreply.github.com>
2023-12-14 17:38:06 +0530

    saperate two ways to use Namespace

--- Lec2_namespace.cpp
+++ Lec2_namespace.cpp
@@ -2,25 +2,25 @@
 
 //----------------CODE SAMPLE 1-------------------
 
-// #include<iostream>
-// int main()
-//{
+ #include<iostream>
+ int main()
+{
             //indicates that the name cout and cin are defined inside the Namespace "std" Library
-//         std::cout<<"Enter the Number\n";
-//         int c1;
-//         std::cin >> c1;
-//         std::cout<<"The Entered No is"<<c1;
-//         return 0;
-//}
+         std::cout<<"Enter the Number\n";
+         int c1;
+         std::cin >> c1;
+         std::cout<<"The Entered No is"<<c1;
+         return 0;
+}
 
 //-------------CODE SAMPLE 2--------------------
 // Namespace "Using" Declaration
 
-#include<iostream>
-// here we declared that std library
-// using "using" declaration for namespace (globally)
-using namespace std;
-int main()
-{
-    cout<<"nameste Duniya"<<endl;
-}
+//#include<iostream>
+//// here we declared that std library
+//// using "using" declaration for namespace (globally)
+//using namespace std;
+//int main()
+//{
+//    cout<<"nameste Duniya"<<endl;
+//}
