b818ade14e1b88a664a225d972d97c4bc84dd018
Ayush Chaware <118629631+ayushchaware08@users.noreply.github.com>
2023-12-14 17:28:26 +0530

    Day 1 : 14/12/2023

---
+++ Lec2_1st_Cpp_prgm.cpp
@@ -0,0 +1,28 @@
+//Iostream  is library where all the functions are defined
+#include <iostream>
+//C++ me The library is defined in a namespace called std
+// here we allready mentioned that we are using Std library in whole code   [see Lec2_namespace.cpp ]
+using namespace std;
+
+int main()
+{
+    // cout is just like printf in C
+
+    // // here we use std::cout that means use cout from std Library
+    // // scope operator (::) 
+    // std::cout<<"Hello World\n";
+    
+    // //to print new line
+    // cout<<"N1"<<endl;
+    // cout<<"N2"<<'\n';
+    // cout<<"N3 \n";
+    // cout<<"\n>";
+
+    // DATA TYPE 
+    int a = 1;
+    cout<< a<< endl;
+
+    // Count Size of a data Type
+    
+}
+
---
+++ Lec2_namespace.cpp
@@ -0,0 +1,23 @@
+//THE Std NAMESPACE 
+
+// #include<iostream>
+// int main()
+//     {
+            //indicates that the name cout and cin are defined inside the Nmaespace Named std Library
+//         std::cout<<"Enter the Number\n";
+//         int c1;
+//         std::cin >> c1;
+//         std::cout<<"The Entered No is"<<c1;
+//         return 0;
+//     }
+
+// Namespace "Using" Declaration
+
+#include<iostream>
+// here we declared that std library
+// using "using" declaration for namespace 
+using namespace std;
+int main()
+{
+    cout<<"nameste Duniya"<<endl;
+}
