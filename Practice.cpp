36894e1905cbc2f1b4180818e54337c58ab0f40a
Ayush Chaware <ayushchaware08@gmail.com>
2023-12-20 23:45:52 +0530

    Day 7: 20/12/2023

--- Practice.cpp
+++ Practice.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
+// convert dedc into binary
+int n;
+	cout << "Enter Decimal Number : ";
+	cin >> n;
+
+	int ans = 0;
+	int i = 0;
+	while (n != 0)
+	{
+		int bit = n & 1;
+		ans = (bit * pow(10, i)) + ans;
+		n = n >> 1;
+		i++;
+	}
+	cout << "Answer is " << ans << endl;
+
+    /*
 	int n;
 	cout<<"ENter : ";
 	cin>>n;
@@ -18,6 +36,9 @@ int main()
 		cout<<endl;
 		i=i+1;
 	}
+    */
+
+
 }
 
 /*
