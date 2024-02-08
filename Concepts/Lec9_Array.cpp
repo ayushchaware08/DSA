13ab2752b3b7da7a20d74aaa3ab9f7967063dbb8
Ayush Chaware <ayushchaware08@gmail.com>
2024-02-09 03:43:11 +0530

    Updating th File

--- Concepts/Lec9_Array.cpp
+++ Concepts/Lec9_Array.cpp
@@ -12,7 +12,7 @@ void printarray(int arr[], int size)
 	{
 		cout << arr[i] << " ";
 	}
-	cout << "Printing Done" << endl;
+	cout << endl<<  "Printing Done" << endl;
 }
 
 int main()
@@ -25,14 +25,14 @@ int main()
 	cout << "value at index 1 is " << first[1] << endl;
 	cout << "value at index 2 is " << first[2] << endl;
 
-	/*---*/
+//  ---------------------------------------------------
 
-	// size = 10; elements = 2; else garbage value
+	// size = 10; elements = 2; else will garbage value
 	int x = 10;
 	int second[10] = {1, 2};
 	printarray(second, x);
 
-	/*---*/
+// ---------------------------------------------------
 
 	// Initialization
 	int n = 5; // size of arr
@@ -50,14 +50,12 @@ int main()
 	int sizeofarray = sizeof(third) / sizeof(int);
 	cout << "Size of array is " << sizeofarray << endl;
 
-	/*---*/
+// ---------------------------------------------------
 
 	// array having Character element
 	int size = 5;
 	char ch1[5] = { 'a', 'b', 'c', 'd', 'e'};
 	// printarray(ch1,size); // printarray(int, int)
-
-
 
 	return 0;
 }
